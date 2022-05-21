e6c3d64f9041484aa9c03a3a250627fb06c52004
Carlos-Vitor <carlosvitor179@protonmail.com>
2022-05-20 22:58:37 -0300

    Alterações em forma

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-var botaoApresentar = document.querySelector("#botaoApresentar")
-var divPainelExibicao = document.querySelector(".containerExibicao")
 
+/*Container da textarea e a seleção do checkbox*/
 var checkBoxPainel = document.querySelector("#habilitarPainel")
 var divPainel = document.querySelector(".containerPainel")
 
-checkBoxPainel.addEventListener("click", function(){
-   
+/*botão final de envio dos dados */
+var botaoApresentar = document.querySelector("#botaoApresentar")
+
+/*Container da listagem de dados e botão para fechar listagem*/
+var containerModal = document.querySelector(".containerModal")
+var botaoFecharModal = document.querySelector("#botaoFecharModal")
+
+ /*Pegando os valores dos inputs do form e definindo nas tags de apresentação dos dados*/
+
+const formularioGeral = document.querySelector("form")
+document.querySelector("form").addEventListener("submit", (evento)=>{
+    evento.preventDefault()
+
+    const formulario = evento.target
+    const formNascimento = Intl.DateTimeFormat("pt-BR", {timeZone: "UTC",}).format(new Date(formulario.nascimento.value));
+    
+    document.querySelector("#listarNome").innerHTML = `Nome: ${formulario.nome.value}`  
+    document.querySelector("#listarSexo").innerHTML = `Sexo: ${ formulario.idade.value}` 
+    document.querySelector("#listarIdade").innerHTML = `Sexo: ${formulario.sexo.value}`
+    document.querySelector("#listarNascimento").innerHTML = `Data Nascimento: ${formNascimento}`
+    document.querySelector("#listarTotalPalavras").innerHTML = `Total de Palavras: ${contarTexto(formulario.TextoArea.value)}`
+
+    /*Limpar todos os inputs do formulario*/
+    formulario.reset()
+
+})
+
+checkBoxPainel.addEventListener("click", ()=>{
+    /*Evento criado ao ativar/desativar o checkbox fazendo aparecer o painel da area de texto*/
     if(checkBoxPainel.checked){
         divPainel.style.display = "block"
     }
@@ -15,73 +41,35 @@ checkBoxPainel.addEventListener("click", function(){
 
 })
 
-botaoApresentar.addEventListener("click" , function(){
-
-    
-    if(divPainelExibicao.style.display != "block"){
-        divPainelExibicao.style.display = "block"
+formularioGeral.addEventListener("submit" , ()=>{
+    if(containerModal.style.display != "none"){
+        containerModal.style.display = "none"
     }
     else{
-        divPainelExibicao.style.display = "none"
+        containerModal.style.display = "flex"
     }
 
 })
 
-botaoApresentar.addEventListener("click" , function(e){
-    e.preventDefault();
+botaoFecharModal.addEventListener("click" , ()=>{
+    if(containerModal.style.display != "none"){
+        containerModal.style.display = "none"
+    }
     
-    /* Pegar valor do formulario*/
-    var formNome = document.querySelector("#nome").value
-    var formIdade = document.querySelector("#idade").value
-    var formSex = document.querySelector('input[name="escolher"]:checked').value
-    var formTexto = document.querySelector("#textoPainel").value
-    
-    /*Pegar valor dos elementos da lista*/
-    var listarNome = document.querySelector("#listarNome")
-    var listarSexo = document.querySelector("#listarSexo")
-    var listarIdade = document.querySelector("#listarIdade")
-    var listarNascimento = document.querySelector("#listarNascimento")
-    var listarTotalPalavras = document.querySelector("#listarTotalPalavras")
 
-    var formulario = document.querySelector("form").value
-    
-    /*pegar total de palavras*/
-    var conversao = contarTexto()
-
-    /*coletar valor do formulario e adicionar ao objeto Date*/
-    var dataConversor = new Date(document.querySelector("input[type=date]").value);         
-
-    listarNome.innerHTML += formNome
-    /*definir valor para timezone UTC e formatar exibição para padrão pt-br */
-    listarNascimento.innerHTML += Intl.DateTimeFormat("pt-BR", {timeZone: "UTC",}).format(dataConversor);
-    listarIdade.innerHTML += formIdade
-    listarSexo.innerHTML += formSex
-    listarTotalPalavras.innerHTML += conversao 
-       
-    formulario.reset()
-
-   
 })
 
-function contarTexto(){
+function contarTexto(contadorTexto){
     /*Função contar total de espaços em branco e retorna o total de palavras*/
 
-    var contadorTexto = document.querySelector("#textoPainel").value
     contadorTexto = contadorTexto.replace(/(^\s*)|(\s*$)/gi,"");
 	contadorTexto = contadorTexto.replace(/[ ]{2,}/gi," ");
 	contadorTexto = contadorTexto.replace(/\n /,"\n");
     
     return contadorTexto.split(' ').filter(function(str){return str!="";}).length;
     
-    
-    
-    
-    /*var conversorTexto = formTexto.split(" ")
-    var conversorTextoVirgula = formTexto.split(", ")
-    var conversorPonto = formTexto.split(". ")
-    var tamanhoTexto = conversorTexto.length
-    return tamanhoTexto*/
 }
+
 
 
 
