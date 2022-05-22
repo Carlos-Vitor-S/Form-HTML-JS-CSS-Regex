f33e51495e59e6f3cbb95049c75d9929edbc8f0e
Carlos-Vitor <carlosvitor179@protonmail.com>
2022-05-22 12:15:03 -0300

    contagem de palavras mais completa

--- script.js
+++ script.js
@@ -15,23 +15,38 @@ var botaoFecharModal = document.querySelector("#botaoFecharModal")
 const formularioGeral = document.querySelector("form")
 document.querySelector("form").addEventListener("submit", (evento)=>{
     evento.preventDefault()
+    
+   
 
     const formulario = evento.target
+    var texto1 = document.querySelector("#textoPainel").value
+   
     const formNascimento = Intl.DateTimeFormat("pt-BR", {timeZone: "UTC",}).format(new Date(formulario.nascimento.value));
     
     document.querySelector("#listarNome").innerHTML = `Nome: ${formulario.nome.value}`  
-    document.querySelector("#listarSexo").innerHTML = `Sexo: ${ formulario.idade.value}` 
+    document.querySelector("#listarSexo").innerHTML = `Sexo: ${formulario.idade.value}` 
     document.querySelector("#listarIdade").innerHTML = `Sexo: ${formulario.sexo.value}`
     document.querySelector("#listarNascimento").innerHTML = `Data Nascimento: ${formNascimento}`
-    document.querySelector("#listarTotalPalavras").innerHTML = `Total de Palavras: ${contarTexto(formulario.TextoArea.value)}`
+    //document.querySelector("#listarTotalPalavras").innerHTML = `Total de Palavras: ${contarTexto(formulario.TextoArea.value)}`
 
-    /*Limpar todos os inputs do formulario*/
-    formulario.reset()
+    var totalTexto = contarTexto(formulario.texto.value)
+    document.querySelector("#listarTotalPalavras").innerHTML = `Total de Palavras: ${totalTexto}`
+    
+    if (totalTexto!=null){
+        
+        var totalExcessoes = contarExcessoesTextuais(texto1)
+        document.querySelector("#listarTotalPalavras").innerHTML = `Total de Palavras: ${totalTexto - totalExcessoes}`
+    }
+
+
+    
+    
+   
 
 })
 
-checkBoxPainel.addEventListener("click", ()=>{
-    /*Evento criado ao ativar/desativar o checkbox fazendo aparecer o painel da area de texto*/
+/*checkBoxPainel.addEventListener("click", ()=>{
+   
     if(checkBoxPainel.checked){
         divPainel.style.display = "block"
     }
@@ -40,6 +55,19 @@ checkBoxPainel.addEventListener("click", ()=>{
     }
 
 })
+*/
+botaoApresentar.addEventListener("click" , ()=>{
+     /*Evento criado ao ativar/desativar o checkbox fazendo aparecer o painel da area de texto*/
+    if(checkBoxPainel.checked){
+        divPainel.style.display = "block"
+    }
+    else {
+        divPainel.style.display = "none"
+    }
+})
+
+
+
 
 formularioGeral.addEventListener("submit" , ()=>{
     if(containerModal.style.display != "none"){
@@ -48,8 +76,10 @@ formularioGeral.addEventListener("submit" , ()=>{
     else{
         containerModal.style.display = "flex"
     }
-
+    formulario.reset()
 })
+
+
 
 botaoFecharModal.addEventListener("click" , ()=>{
     if(containerModal.style.display != "none"){
@@ -61,7 +91,7 @@ botaoFecharModal.addEventListener("click" , ()=>{
 
 function contarTexto(contadorTexto){
     /*Função contar total de espaços em branco e retorna o total de palavras*/
-
+    
     contadorTexto = contadorTexto.replace(/(^\s*)|(\s*$)/gi,"");
 	contadorTexto = contadorTexto.replace(/[ ]{2,}/gi," ");
 	contadorTexto = contadorTexto.replace(/\n /,"\n");
@@ -69,6 +99,27 @@ function contarTexto(contadorTexto){
     return contadorTexto.split(' ').filter(function(str){return str!="";}).length;
     
 }
+
+
+function contarExcessoesTextuais(textoExcessoes){
+
+    // \d+ = digitos numericos
+    // \[%#@$&*'"~^´`] = caracteres especiais
+    // \b[b-df-hj-np-tv-z]+\b = consoantes
+
+    textoExcessoes.split(/[,.?!\s/]+/)
+  
+    textoExcessoes = textoExcessoes.match(/\d+|[%#@$&*'"~^`]+|\b[b-df-hj-np-tv-z]+\b/gi)
+
+    //retorna o total de excessoes
+    
+    return textoExcessoes.length
+    
+      
+}
+
+
+
 
 
 
